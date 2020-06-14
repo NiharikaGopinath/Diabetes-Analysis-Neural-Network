fed9f71f877cb544fd7ad7dd6d583c77e2eca889
Niharika Menon <59258102+NiharikaGopinath@users.noreply.github.com>
2020-06-14 13:48:17 +0530

    Add files via upload

---
+++ diabetesanalysis.py
@@ -0,0 +1,17 @@
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+dataset = loadtxt('pima-indians-diabetes.csv',delimiter=',')
+X = dataset[:, 0:8]
+y = dataset[:, 8]
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=150, batch_size=10, verbose=0)
+predictions = model.predict_classes(X)
+for i in range(5):
+	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
+
